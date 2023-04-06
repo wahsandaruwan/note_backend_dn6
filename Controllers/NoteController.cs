5589995b68c97fda592ba6f847b6643e38f65574
wahsandaruwan <wahsandaruwan6@gmail.com>
2023-04-06 15:41:02 +0530

    Added simple routing part

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -11,11 +11,19 @@ namespace backend.Controllers
     // Controller for the note
     public class NoteController : ControllerBase
     {
-        private static readonly Note new1 = new();
+        private static readonly List<Note> notes = new List<Note>{
+            new Note(),
+            new Note {Title = "Sample Note"}
+        };
 
-        [HttpGet]
-        public ActionResult<Note> Get(){
-            return Ok(new1);
+        [HttpGet("GetAll")]
+        public ActionResult<List<Note>> GetAll(){
+            return Ok(notes);
+        }
+
+        [HttpGet("GetSingle")]
+        public ActionResult<Note> GetSingle(){
+            return Ok(notes[0]);
         }
     }
 }
